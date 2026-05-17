4e3b221b9cda06878d1d570ea7773025daffbfe7
James Peckham <jhered@yahoo.com>
2026-05-17 15:08:50 -0500

    feat: add FindByHandleAsync, HandleExistsAsync, UpdateAsync to IUserRepository

--- src/SocialDDD.Domain/Users/IUserRepository.cs
+++ src/SocialDDD.Domain/Users/IUserRepository.cs
@@ -6,7 +6,10 @@ public interface IUserRepository
     Task<User?> GetByIdAsync(UserId id, CancellationToken ct = default);
     Task<User?> GetByEmailAsync(Email email, CancellationToken ct = default);
     Task<User?> GetByUsernameAsync(Username username, CancellationToken ct = default);
+    Task<User?> FindByHandleAsync(Handle handle, CancellationToken ct = default);
     Task<bool> ExistsByEmailAsync(Email email, CancellationToken ct = default);
     Task<bool> ExistsByUsernameAsync(Username username, CancellationToken ct = default);
     Task<bool> ExistsByIdAsync(UserId id, CancellationToken ct = default);
+    Task<bool> HandleExistsAsync(Handle handle, CancellationToken ct = default);
+    Task UpdateAsync(User user, CancellationToken ct = default);
 }
